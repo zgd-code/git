9ee135d88ce45ffffb07239178afd2f77748d269
ZGD <ZGD@huali.com>
2022-05-27 11:15:01 +0800

    first-commit

---
+++ src/main/java/com/huali/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.huali.git;
+
+/**
+ * @author ZGD
+ * @create 2022--05--27--11:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello!Git!");
+    }
+}
